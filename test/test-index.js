d807b2d2740ef3d0eadead78dd9006e8529e0ef4
Pim <pim@zifzaf.com>
2018-11-04 17:51:24 +0100

    void test

--- test/test-index.js
+++ test/test-index.js
@@ -1,16 +1,18 @@
 'use strict';
 
+/*
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 const assert = require('chai').assert;
-const server = require('../bin/index.js');
+const server = require('../index.js');
+*/
 
 suite('Get home page', function () {
-    test('get by \'/\'', function (done) {
-        let err = null;
+  test('get by \'/\'', function (done) {
+    let err = null;
 
-        done(err);
-    });
+    done(err);
+  });
 });
