9cb72cae5a6c9b5afe9d1e35a2ef98c6f550be49
Pim <pim@zifzaf.com>
2018-11-04 16:19:34 +0100

    tests added

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.redirect('index.html');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running at localhost: ' + app.get('port'));
 });
---
+++ test/test-index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const assert = require('chai').assert;
+const server = require('../bin/index.js');
+
+suite('Get home page', function () {
+    test('get by \'/\'', function (done) {
+        let err = null;
+
+        done(err);
+    });
+});
